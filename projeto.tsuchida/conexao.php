7385e1f9c7560f17245a9c4ab6a6eb911fbc7414
tsuchidamay <165052661+tsuchidamay@users.noreply.github.com>
2025-08-28 11:42:05 -0300

    Colocado os códigos

---
+++ projeto.tsuchida/conexao.php
@@ -0,0 +1,18 @@
+<?php
+
+// Configurações do banco
+$host    = "localhost";   // normalmente não precisa alterar
+$usuario = "root";        // substituir se seu usuário não for root
+$senha   = "";            // substituir se você tiver senha no MySQL
+$banco   = "projeto.tsuchida";       // substituir pelo nome do seu banco criado no phpMyAdmin
+
+// Conexão MySQLi
+$conexao = mysqli_connect($host, $usuario, $senha, $banco);
+
+if (!$conexao) {
+    die("Erro ao conectar: " . mysqli_connect_error());
+}
+
+// SENSITIVE CASE suportar acentos e Ç
+mysqli_set_charset($conexao, "utf8");
+?>
